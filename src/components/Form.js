b93fd21ae6e6cdcb872d06bb0b927cc9b11ace0d
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2019-12-18 11:15:17 -0500

    code challenge

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,40 @@
 import React from "react";
 
 class Form extends React.Component {
+
+constructor(){
+  super();
+  this.state= {
+    title: "Enter book title here",
+    author: "Enter author here",
+    img: "Enter url for image here"
+
+  }
+}
+
+addBook= (event) => {
+  event.preventDefault();
+ this.setState({
+   [event.target.name]: event.target.name.value
+
+ })
+ 
+}
+
+
   render() {
-    return <h1>{/*create form*/}</h1>;
+    
+    return (<div> <h2>Log a Book</h2>
+    
+  <form>
+  <input type="text" name= "title" value= {this.props.title} onChange={() => this.addBook()} />
+  <input type="text" name= "author" value= {this.props.author} onChange={() => this.addBook()}/>
+  <input type="text" name= "img" value={this.props.img} onChange={() => this.addBook()}/>
+  <input type="submit" name= "submit" value="submit" onClick={() => this.props.onSubmit()}/>
+    </form>
+    </div>
+   
+    )
   }
 }
 
--- src/containers/Bookshelf.js
+++ src/containers/Bookshelf.js
@@ -2,10 +2,23 @@ import React from "react";
 import Book from "../components/Book";
 
 const Bookshelf = props => {
+  
+  console.log(props.selectedBooks)
+
+  const renderSelectedBooks= () => {
+    return "something"
+    // return props.selectedBooks.map(book => {
+    //   return <img src= {book.img} />
+    // })
+  }
+
+
+
   return (
     <div>
       <h1>Book Shelf</h1>
-      <ul>{/*render list of books here*/}</ul>
+      <ul>Render selected books here  <li onclick={() => this.props.unselectBook(props)}> {renderSelectedBooks()} </li></ul>
+     
     </div>
   );
 };

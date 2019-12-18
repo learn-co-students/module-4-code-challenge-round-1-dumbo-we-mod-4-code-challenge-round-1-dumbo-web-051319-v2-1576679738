50a558be2371f517ab97b0a96195c51f4d289676
Muna Mashhour <munamashhour@gmail.com>
2019-12-18 10:25:50 -0500

    submitting before i break something

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,42 @@
 import React from "react";
 
 class Form extends React.Component {
+
+  state = {
+    title: null,
+    author: null,
+    img: null,
+    id: Math.floor(Math.random() * (92) + 9)
+  }
+
+  handleChange = (event) => {
+   this.setState({
+     [event.target.name]: event.target.value 
+   })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addBook({
+      title: this.state.title, 
+      author: this.state.author,
+      img: this.state.img,
+      id: this.state.id 
+    })
+  }
+
   render() {
-    return <h1>{/*create form*/}</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input type="text" placeholder="enter book title" name="title" onChange={this.handleChange}/>
+        <label htmlFor="author">Author:</label>
+        <input type="text" placeholder="enter book author" name="author" onChange={this.handleChange}/>
+        <label htmlFor="img">Image:</label>
+        <input type="text" placeholder="enter book image" name="img" onChange={this.handleChange}/>
+        <button>Submit</button>
+      </form>
+    )
   }
 }
 
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,12 +3,19 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
+
+  renderBooks = () => {
+    return this.props.books.map(book => 
+      <Book id={book.id} book={book} handleClick={this.props.handleClick}/>
+    )
+  }
+
   render() {
     return (
       <div className="book-list">
         <h1>Book List</h1>
-        <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <Form addBook={this.props.addBook}/>
+          <ul>{this.renderBooks()}</ul>
       </div>
     );
   }

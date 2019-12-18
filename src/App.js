6f7c0f68f40254b25baeacb88b667a908384f6c6
digitalflee <keenan.alves@gmail.com>
2019-12-18 10:50:01 -0500

    try 1

--- src/App.js
+++ src/App.js
@@ -5,11 +5,45 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  state = {
+    books: [],
+    bookShelf: []
+  }
+
+  componentDidMount(){
+    fetch(`http://localhost:3005/books`)
+    .then(r => r.json())
+    .then((allBooks) => {
+     this.setState({
+       books: allBooks
+     })
+    })
+  }
+
+  addToShelf = (book) => {
+    console.log('adding', book)
+   let copyBooks = this.state.books
+   copyBooks = []
+   copyBooks.push(book)
+    
+    this.setState({
+      bookShelf: copyBooks
+    })
+  }
+  
+  removeFromShelf = (book) => {
+    console.log('removing')
+
+  }
+
+
   render() {
+    
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList books={this.state.books} addToShelf={this.addToShelf}/>
+        <Bookshelf books={this.state.bookShelf} removeFromShelf={this.removeFromShelf}/>
       </div>
     );
   }
--- src/containers/Bookshelf.js
+++ src/containers/Bookshelf.js
@@ -1,13 +1,24 @@
-import React from "react";
+
+import React, { Component } from 'react'
 import Book from "../components/Book";
 
-const Bookshelf = props => {
-  return (
-    <div>
-      <h1>Book Shelf</h1>
-      <ul>{/*render list of books here*/}</ul>
-    </div>
-  );
-};
+export class Bookshelf extends Component {
 
-export default Bookshelf;
+
+
+  
+  render() {
+    
+    return (
+      <div>
+        <h1>Book Shelf</h1>
+    <ul>{this.props.books.map(book => <Book book={book} key={book.id} addToShelf={this.props.addToShelf} removeFromShelf={this.props.removeFromShelf}/>)}
+</ul>
+      </div>
+    )
+  }
+}
+
+export default Bookshelf
+
+// {this.props.books.map(book => <Book book={book} key={book.id}/>)}

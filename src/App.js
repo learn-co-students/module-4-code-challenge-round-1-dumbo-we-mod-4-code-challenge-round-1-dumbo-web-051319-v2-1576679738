970cc00581025283990c40e19a626bd83aea9bdb
AlinaArakelyan <alinaarakelyan90@gmail.com>
2019-12-18 11:16:45 -0500

    done

--- src/App.js
+++ src/App.js
@@ -5,11 +5,51 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  state = {
+    books: [],
+    shelf: []
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3005/books`)
+      .then(r => r.json())
+      .then((booksArr) => {
+        this.setState({
+          books: booksArr
+        })
+      }
+    )
+  }
+
+  addToShelf = (bookObj) => {
+    this.setState({
+      shelf: [...this.state.shelf, bookObj]
+    })
+  }
+
+  removeFromShelf = (bookObj) => {
+      this.setState({
+        shelf: this.state.shelf.filter(book => book.id !== bookObj.id)
+      })
+
+  }
+
+  addNewBook = (evt, bookObj) => {
+    // console.log(evt, bookObj)
+    evt.preventDefault()
+    this.setState({
+      books: [...this.state.books, bookObj]
+    })
+  }
+
+
   render() {
+    // console.log(this.state.books)
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList books={this.state.books} onClick={this.addToShelf} handleSubmit={this.addNewBook}/>
+        <Bookshelf books={this.state.shelf} onClick={this.removeFromShelf}/>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 class Form extends React.Component {
+
+
+  state = {
+    title: " ",
+    author: " ",
+    img: " "
+  }
+
+
+
+  handleInputChange = (evt) => {
+    const value = evt.target.value;
+    const name = evt.target.name;
+
+
+    this.setState({
+      [name]: value
+    })
+
+  }
+
+
+
+
   render() {
-    return <h1>{/*create form*/}</h1>;
+    return <h1>{
+      <form onSubmit={(evt) => this.props.handleSubmit(evt, this.state)}>
+        <label> Title: </label><input type="text" name="title" value={this.state.title} onChange={this.handleInputChange}></input>
+        <label> Author: </label><input type="text" name="author" value={this.state.author} onChange={this.handleInputChange}></input>
+        <label> Image: </label><input type="text" name="img" value={this.state.img} onChange={this.handleInputChange} ></input>
+        <button type="submit" value="submit">submit</button>
+      </form>
+    }</h1>;
   }
 }
 

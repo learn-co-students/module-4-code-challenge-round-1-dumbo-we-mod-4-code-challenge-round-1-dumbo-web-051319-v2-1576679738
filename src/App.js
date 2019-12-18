ee81885974178a8dbccd0cfc11798c28bec3fb00
github username <github email address>
2019-12-18 10:58:33 -0500

    basic deliverables finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  state = {
+    books: [],
+    bookShelf: [],
+    newBook: {}
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3005/books`)
+      .then(r => r.json())
+      .then(books => this.setState({books: books}))
+      .catch(err => console.log(err))
+  }
+
+  addBookToBookShelf = (book) => {
+    const newlyOwnedBook = {...book}
+    const uuidv4 = require('uuid/v4')
+    newlyOwnedBook.personalID = uuidv4()
+    this.setState({bookShelf: [newlyOwnedBook, ...this.state.bookShelf]})
+  }
+
+  removeBookFromBookShelf = (bookToRemove => {
+    this.setState({bookShelf: this.state.bookShelf.filter(book => book.personalID !== bookToRemove.personalID)})
+  })
+
+  addNewBookToBooks = (newBook) => {
+    // event.preventDefault()
+    const newlyCreatedBook = {...newBook}
+    const uuidv4 = require('uuid/v4')
+    newlyCreatedBook.id = uuidv4()
+    this.setState({books: [newlyCreatedBook, ...this.state.books]})
+  }
+
   render() {
+    // console.log(this.state)
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList 
+          books={this.state.books}
+          addBookToShelf={this.addBookToBookShelf}
+          addNewBookToBooks={this.addNewBookToBooks}
+        />
+        <Bookshelf 
+          books={this.state.bookShelf}
+          removeBookFromBookShelf={this.removeBookFromBookShelf}
+        />
       </div>
     );
   }
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,12 +3,18 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
+
+  renderBooks = () => {
+    return this.props.books.map(book => <Book book={book} onClick={this.props.addBookToShelf} key={book.id}/>)
+  }
+  
+
   render() {
     return (
       <div className="book-list">
         <h1>Book List</h1>
-        <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <Form addNewBookToBooks={this.props.addNewBookToBooks}/>
+        <ul>{this.renderBooks()}</ul>
       </div>
     );
   }
